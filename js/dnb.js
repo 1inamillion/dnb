d1005a770517f6242881cf68a36845e71ac29f55
1inamillion <1inamillion1000000@gmail.com>
2017-01-11 14:18:13 +0900

    minify その２

--- js/dnb.js
+++ js/dnb.js
@@ -4,13 +4,12 @@ $(function () {$(".modal").modal(),$(".album-info").hide(),$(".live-info").hide(
       $(".album-info").append(_html);    
 $(window).scroll(function () {
     scr_count = $(document).scrollTop();
-//         console.log(scr_count);
+
     if (scr_count > first_pos && ALBUM_ON == false) {
-            _html = (function () {/*
-            <br>
+            _html = "<br>
             <p>1inamillion Presents</p>
-            <p>"DYNAMITE NAMA-BEER" RELEASE PARTY</p>
-            <h5 style="font-weight:900;">PLAYLAND</h5>
+            <p>DYNAMITE NAMA-BEER RELEASE PARTY</p>
+            <h5 style='font-weight:900;'>PLAYLAND</h5>
             <p><b>2017.02.04 at KICHIJOJI-WARP</b></p>
             <br>
             <p>1inamillion</p>
@@ -20,8 +19,7 @@ $(window).scroll(function () {
             <p>AGATHA</p>
             <br>
             <p>OPEN 17:30 / START 18:00</p>
-            <p>Admission Fee / <span style="font-style:italic;">2,000 JPY</span></p>            
-            */}).toString().replace(/(\n)/g, '').split('*')[1];
+            <p>Admission Fee / <span style="font-style:italic;">2,000 JPY</span></p>";
             $(".live-info").append(_html);
             $('.album-info').show();
             ALBUM_ON = true;
@@ -34,97 +32,93 @@ $(window).scroll(function () {
         }
 
         if (scr_count > third_pos && MOVIE_ON == false) {
-            _html = (function () {/*
-            <br>
-            <div class="col s12 m12">
-                <div class="card">
-                <div class="card-image">
-                    <img src="img/1inamillion.jpg">
-                    <span class="card-title"><b>1inamillion</b></span>
+            _html = "<br>
+            <div class='col s12 m12'>
+                <div class='card'>
+                <div class='card-image'>
+                    <img src='img/1inamillion.jpg'>
+                    <span class='card-title'><b>1inamillion</b></span>
                 </div>
-                <div class="card-content">
+                <div class='card-content'>
                     <p>Instrumental emo post rock band from Tokyo, Japan.</p>
                     <p>We'll show you amazing live performance and excited power of band sound without HUMAN VOICE!</p>
                     <p>Please watch our live videos from links below, and follow us on Twitter if you get interested.</p>            
                 </div>
-                <div class="card-action right-align">
-                    <a class="btn blue" href="https://twitter.com/1inamillion_jpn" target="_blank">TWITTER</a>            
-                    <a class="waves-effect waves-light btn red" href="https://youtu.be/i45XqfyOflg" target="_blank">VIDEO</a>
+                <div class='card-action right-align'>
+                    <a class='btn blue' href='https://twitter.com/1inamillion_jpn' target='_blank'>TWITTER</a>            
+                    <a class='waves-effect waves-light btn red' href='https://youtu.be/i45XqfyOflg' target='_blank'>VIDEO</a>
                 </div>
                 </div>
             </div>
 
-            <div class="col s12 m6">
-                <div class="card">
-                <div class="card-image">
-                    <img src="img/carnival.jpg">
-                    <span class="card-title"><b>CARNIVAL</b></span>
+            <div class='col s12 m6'>
+                <div class='card'>
+                <div class='card-image'>
+                    <img src='img/carnival.jpg'>
+                    <span class='card-title'><b>CARNIVAL</b></span>
                 </div>
-                <div class="card-content">
+                <div class='card-content'>
                     <p>The most emotional instrumental rock band in Kichijoji, Japan. </p>
                 </div>
-                <div class="card-action right-align">
-                    <a class="waves-effect waves-light btn red" href="https://youtu.be/VAgRzRGbsWE" target="_blank">VIDEO</a>
+                <div class='card-action right-align'>
+                    <a class='waves-effect waves-light btn red' href='https://youtu.be/VAgRzRGbsWE' target='_blank'>VIDEO</a>
                 </div>
                 </div>
             </div>
 
-            <div class="col s12 m6">
-                <div class="card">
-                <div class="card-image">
-                    <img src="img/sprawl.jpg">
-                    <span class="card-title"><b>sprawl</b></span>
+            <div class='col s12 m6'>
+                <div class='card'>
+                <div class='card-image'>
+                    <img src='img/sprawl.jpg'>
+                    <span class='card-title'><b>sprawl</b></span>
                 </div>
-                <div class="card-content">
+                <div class='card-content'>
                     <p>All the members of this band are always friendly and affluence.</p>
                 </div>
-                <div class="card-action right-align">
-                    <a class="waves-effect waves-light btn red" href="https://youtu.be/EosGz97fqEY" target="_blank">VIDEO</a>          </div>
+                <div class='card-action right-align'>
+                    <a class='waves-effect waves-light btn red' href='https://youtu.be/EosGz97fqEY' target='_blank'>VIDEO</a>          </div>
                 </div>
             </div>
 
-            <div class="col s12 m6">
-                <div class="card">
-                <div class="card-image">
-                    <img src="img/mantle.jpg">
-                    <span class="card-title"><b>the mantle brothers</b></span>
+            <div class='col s12 m6'>
+                <div class='card'>
+                <div class='card-image'>
+                    <img src='img/mantle.jpg'>
+                    <span class='card-title'><b>the mantle brothers</b></span>
                 </div>
-                <div class="card-content">
+                <div class='card-content'>
                     <p>Minimal but giving you fun a lot, let's dance with them!</p>
                 </div>
-                <div class="card-action right-align">
-                    <a class="waves-effect waves-light btn red" href="https://youtu.be/TBL6YS_KtEo" target="_blank">VIDEO</a>
+                <div class='card-action right-align'>
+                    <a class='waves-effect waves-light btn red' href='https://youtu.be/TBL6YS_KtEo' target='_blank'>VIDEO</a>
                 </div>
                 </div>
             </div>
 
-            <div class="col s12 m6">
-                <div class="card">
-                <div class="card-image">
-                    <img src="img/agatha.jpg">
-                    <span class="card-title"><b>AGATHA</b></span>
+            <div class='col s12 m6'>
+                <div class='card'>
+                <div class='card-image'>
+                    <img src='img/agatha.jpg'>
+                    <span class='card-title'><b>AGATHA</b></span>
                 </div>
-                <div class="card-content">
+                <div class='card-content'>
                     <p>Crazy talented instrumental math-rock trio from Kita-Senju, Tokyo.</p>
                 </div>
-                <div class="card-action right-align">
-                    <a class="waves-effect waves-light btn red" href="https://youtu.be/Wu8JUxB7a3k" target="_blank">VIDEO</a></div>
+                <div class='card-action right-align'>
+                    <a class='waves-effect waves-light btn red' href='https://youtu.be/Wu8JUxB7a3k' target='_blank'>VIDEO</a></div>
                 </div>
-            </div>               
-            */}).toString().replace(/(\n)/g, '').split('*')[1];
+            </div>";
             $(".act-info").append(_html);
             $('.movie-info').show();
             MOVIE_ON = true;
         }
 
         if (scr_count > fourth_pos && ACT_ON == false) {
-            _html = (function () {/*
-            <h5> Thank you for visiting us!</h5>
+            _html = "<h5> Thank you for visiting us!</h5>
             <p>We are looking forward to meeting you at our release party!</p>
             <p>Latest infomation will be announced by our Twitter account,</p>
             <p>please check and follow us.</p>
-            <a href="https://twitter.com/1inamillion_JPN" target="_blank" class="twitter-follow-button" data-show-count="false">@1inamillion_JPN</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> 
-            */}).toString().replace(/(\n)/g, '').split('*')[1];
+            <a href='https://twitter.com/1inamillion_JPN' target='_blank' class='twitter-follow-button' data-show-count='false'>@1inamillion_JPN</a><script async src='https://platform.twitter.com/widgets.js' charset='utf-8'></script>";
             $(".messages").append(_html);
             $('.act-info').show();            
             ACT_ON = true;
